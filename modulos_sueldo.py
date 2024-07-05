99d90c89cda627783a46186a28872abf45243b00
deiby <thefrogzes@gmail.com>
2024-07-05 13:18:59 -0400

    Agregue modulo principal y otras cositas

--- modulos_sueldo.py
+++ modulos_sueldo.py
@@ -1,5 +1,5 @@
 
-def sueldo(horas_dia_scheduled, horas_dia_off, horas_nocturnas, pago_porhora):
+def sueldo(horas_dia_scheduled, horas_dia_off, horas_nocturnas, pago_porhora, deducciones):
 
     Extras = (horas_dia_scheduled - 44) * 1.35
     horas_dia_off = horas_dia_scheduled * 2
@@ -17,24 +17,33 @@ def sueldo(horas_dia_scheduled, horas_dia_off, horas_nocturnas, pago_porhora):
     Sueldo_bruto = Sueldo 
 #Arreglar IRS,se calcula mensual
     if Sueldo_bruto < 34685:
-        sueldo_neto = Sueldo_bruto - (SFS + AFP)
+        sueldo_neto = Sueldo_bruto - (SFS + AFP) - deducciones
         return sueldo_neto
     if Sueldo_bruto >= 34685 and Sueldo_bruto <= 52027.416666667:
-        sueldo_neto = Sueldo_bruto - ((Sueldo_bruto - 34685)*0.15) - (SFS + AFP)
+        sueldo_neto = Sueldo_bruto - ((Sueldo_bruto - 34685)*0.15) - (SFS + AFP) - deducciones
         return sueldo_neto
     if Sueldo_bruto >= 52027.416666667 and Sueldo_bruto <= 72260.25:
         IRS0 = 34685
         IRS15 = 17342.416666667*0.15
         IRS20 = (Sueldo_bruto - IRS0 - 17342.416666667) * 0.20
-        sueldo_neto = Sueldo_bruto - IRS20 - IRS15 - (SFS + AFP)
+        sueldo_neto = Sueldo_bruto - IRS20 - IRS15 - (SFS + AFP) - deducciones
         return sueldo_neto
     if Sueldo_bruto > 72260.25:
         IRS0 = 34685
         IRS15 = 17342.416666667*0.15
         IRS20 = 20232.833333333* 0.20
         IRS25 = (Sueldo_bruto - IRS0 - 17342.416666667 - 20232.833333333) * 0.25
-        sueldo_neto = Sueldo_bruto - IRS25 - IRS20 - IRS15 - (SFS + AFP)
+        sueldo_neto = Sueldo_bruto - IRS25 - IRS20 - IRS15 - (SFS + AFP) - deducciones
         return sueldo_neto  
 
-def deducciones_adicionales (deducciones_quncenales):
-    deduccion = sueldo-deducciones_quncenales
+def informacion():
+    nombre = input ("Ingrese su nombre y apellido: ")
+    pago_por_hora = int(input("Ingrese su pago por hora: "))
+    horas_scheduled = int(input("Ingrese la cantidad de horas normales: "))
+    horas_nocturnas = int(input("Ingrese la cantidad de horas nocturas: "))
+    horas_off = int(input("Ingrese la cantidad de horas en su dia off: "))
+    deduccion = int(input("Ingrese el total de otras deducciones: "))
+
+    sueldo(horas_scheduled, horas_off, horas_nocturnas, pago_por_hora, deduccion)
+    
+    
---
+++ sueldo.py
@@ -0,0 +1,22 @@
+from modulos_sueldo import sueldo, informacion
+
+while True:
+    menu_principal = input("""Calculadora de sueldo, seleccione una opcion:
+
+            1-Calcular sueldo por quincena
+            2-Calcular sueldo mensual
+            3-Calcular doble sueldo\n""")
+    
+    #corregir esto
+    if menu_principal in ("1", "2", "3", "4"):
+        if menu_principal == "1":
+            print (informacion())
+        elif menu_principal == "2":
+            print (informacion())
+        elif menu_principal == "3":
+            print (informacion())
+        else:
+            print ("Elija una opcion correcta:\n")
+
+
+
