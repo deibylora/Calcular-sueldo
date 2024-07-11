3752757469d423416c8cc16fc59ae7b388843ec8
Angel <thefrogzes@gmail.com>
2024-07-10 23:11:49 -0400

    agregue AFP y SFS en el print

--- modulos_sueldo.py
+++ modulos_sueldo.py
@@ -2,12 +2,12 @@
 def sueldo(horas_dia_scheduled, horas_dia_off, horas_nocturnas, pago_porhora, deducciones):
 
     Extras = (horas_dia_scheduled - 44) * 1.35
-    horas_dia_off = horas_dia_scheduled * 2
-    horas_nocturnas = horas_dia_scheduled * 1.15
+    horas_dia_off = horas_dia_off * pago_porhora * 2
+    horas_nocturnas = horas_nocturnas* pago_porhora * 1.15
     if horas_dia_scheduled <= 44:
-        Sueldo = (horas_dia_scheduled * pago_porhora) + (horas_dia_off * pago_porhora) + (horas_nocturnas * pago_porhora)
+        Sueldo = (horas_dia_scheduled * pago_porhora) + (horas_dia_off) + (horas_nocturnas)
     elif horas_dia_scheduled > 44:    
-        Sueldo = (Extras * pago_porhora) + ((horas_dia_scheduled - Extras) * pago_porhora) + (horas_dia_off * pago_porhora) + (horas_nocturnas * pago_porhora)
+        Sueldo = (Extras * pago_porhora) + ((horas_dia_scheduled - Extras) * pago_porhora) + (horas_dia_off) + (horas_nocturnas)
     elif horas_dia_scheduled < 0:
         print ("Error, debes ingresar una cantidad de horas validas, mayor que cero!!")
     else:
@@ -15,35 +15,34 @@ def sueldo(horas_dia_scheduled, horas_dia_off, horas_nocturnas, pago_porhora, de
     SFS = Sueldo * 0.0304
     AFP = Sueldo * 0.0287
     Sueldo_bruto = Sueldo 
-#Arreglar IRS,se calcula mensual
+
     if Sueldo_bruto < 34685:
         sueldo_neto = Sueldo_bruto - (SFS + AFP) - deducciones
-        return sueldo_neto
+        return f"Sueldo neto: {sueldo_neto}, SFS: {SFS}, AFP: {AFP}\n" 
     if Sueldo_bruto >= 34685 and Sueldo_bruto <= 52027.416666667:
         sueldo_neto = Sueldo_bruto - ((Sueldo_bruto - 34685)*0.15) - (SFS + AFP) - deducciones
-        return sueldo_neto
+        return f"Sueldo neto: {sueldo_neto}, SFS: {SFS}, AFP: {AFP}\n" 
     if Sueldo_bruto >= 52027.416666667 and Sueldo_bruto <= 72260.25:
         IRS0 = 34685
         IRS15 = 17342.416666667*0.15
         IRS20 = (Sueldo_bruto - IRS0 - 17342.416666667) * 0.20
         sueldo_neto = Sueldo_bruto - IRS20 - IRS15 - (SFS + AFP) - deducciones
-        return sueldo_neto
+        return f"Sueldo neto: {sueldo_neto}, SFS: {SFS}, AFP: {AFP}\n" 
     if Sueldo_bruto > 72260.25:
         IRS0 = 34685
         IRS15 = 17342.416666667*0.15
         IRS20 = 20232.833333333* 0.20
         IRS25 = (Sueldo_bruto - IRS0 - 17342.416666667 - 20232.833333333) * 0.25
         sueldo_neto = Sueldo_bruto - IRS25 - IRS20 - IRS15 - (SFS + AFP) - deducciones
-        return sueldo_neto  
+
+        return f"Sueldo neto: {sueldo_neto}, SFS: {SFS}, AFP: {AFP}\n" 
 
 def informacion():
-    nombre = input ("Ingrese su nombre y apellido: ")
-    pago_por_hora = int(input("Ingrese su pago por hora: "))
-    horas_scheduled = int(input("Ingrese la cantidad de horas normales: "))
-    horas_nocturnas = int(input("Ingrese la cantidad de horas nocturas: "))
+    pago_hora = int(input("Ingrese su pago por hora: "))
+    horas_normal = int(input("Ingrese la cantidad de horas normales: "))
+    horas_noche = int(input("Ingrese la cantidad de horas nocturas: "))
     horas_off = int(input("Ingrese la cantidad de horas en su dia off: "))
-    deduccion = int(input("Ingrese el total de otras deducciones: "))
+    deduccion = float(input("Ingrese el total de otras deducciones: "))
 
-    sueldo(horas_scheduled, horas_off, horas_nocturnas, pago_por_hora, deduccion)
-    
+    print(sueldo(horas_normal, horas_off, horas_noche, pago_hora, deduccion))
     
--- sueldo.py
+++ sueldo.py
@@ -12,8 +12,23 @@ while True:
         if menu_principal == "1":
             print (informacion())
         elif menu_principal == "2":
-            print (informacion())
-        elif menu_principal == "3":
-            print (informacion())
+            print("Datos de la primera quincena:\n")
+            primeraquincena = informacion()
+            print (f"Pago de primera quincena: {primeraquincena}")
+            print("Datos de la segunda quincena:\n")
+            segundaquincena = informacion()
+            print (f"Pago de segunda quincena: {segundaquincena}")
+            tercerpago = input("Hubo un tercer pago en el mes?: (Y/N)")
+            if tercerpago == "Y":
+                terceraquincena = informacion()
+                print (f"Pago de tercera quincena: {terceraquincena}")
+                totalmensual = primeraquincena + segundaquincena + terceraquincena
+                print(f"Pago mensual: {totalmensual}")
+            elif tercerpago == "N":
+                totalmensual = primeraquincena + segundaquincena
+                print(f"Pago mensual: {totalmensual}")   
+
+        #elif menu_principal == "3":
+        #   print (informacion())
         else:
             print ("Elija una opcion correcta:\n")
